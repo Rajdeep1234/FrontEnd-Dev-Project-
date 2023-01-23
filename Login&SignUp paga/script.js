478e316b09a0bf90acc0387433dd4ae7cfd39779
Rajdeep <rajdeepgupta28011995@gmail.com>
2023-01-23 23:08:11 +0530

    Login Design added

---
+++ Login&SignUp paga/script.js
@@ -0,0 +1,10 @@
+const wrapper=document.querySelector(".wrapper"),
+ signupHeader=document.querySelector(".signup header");
+ loginHeader=document.querySelector(".login header");
+
+ loginHeader.addEventListener("click",()=>{
+    wrapper.classList.add("active");
+ })
+ signupHeader.addEventListener("click",()=>{
+    wrapper.classList.remove("active");
+ })
